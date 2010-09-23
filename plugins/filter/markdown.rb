3ff03207448b8929961c482fa86cd6e0a05abac6
minad <mail@daniel-mendler.de>
2010-09-23 13:12:00 +0200

    fix issue #34. add output parameter for images in markdown filter

--- plugins/filter/markdown.rb
+++ plugins/filter/markdown.rb
@@ -3,5 +3,15 @@ dependencies 'engine/filter'
 require      'rdiscount'
 
 Filter.create :markdown do |context, content|
-  RDiscount.new(content, :filter_html).to_html
+  content = RDiscount.new(content, :filter_html).to_html
+  content.gsub!(/(<img[^>+]src=")([^"]+)"/) do |match|
+    prefix, path = $1, $2
+    if path.begins_with?('http://') || path.begins_with?('https://')
+      match
+    else
+      path = unescape_html(path)
+      prefix + escape_html(path + (path.include?('?') ? '&' : '?') + 'output=image') + '"'
+    end
+  end
+  content
 end
