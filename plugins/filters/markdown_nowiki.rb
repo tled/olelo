6c2c76d9370b742089e326e0de116a84c3791b9e
minad <mail@daniel-mendler.de>
2011-03-19 03:21:39 +0100

    use blank? instead of empty?

--- plugins/filters/markdown_nowiki.rb
+++ plugins/filters/markdown_nowiki.rb
@@ -2,7 +2,7 @@ description 'Markdown nowiki filter'
 
 Filter.create :markdown_nowiki do |context, content|
   output = ''
-  until content.empty?
+  until content.blank?
     case content
     when /(\A( {4}|\t).*)|(\A``.*?``)|(\A`[^`]*`)/
       output << "<notags>#{$&}</notags>"
