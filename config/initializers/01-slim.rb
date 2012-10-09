7402af2fe788c54d941a0917ec9b462d7d93d829
Daniel Mendler <mail@daniel-mendler.de>
2012-10-09 16:39:35 +0200

    Use slim shortcuts for input elements

--- config/initializers/01-slim.rb
+++ config/initializers/01-slim.rb
@@ -1,1 +1,2 @@
-Slim::Engine.set_default_options :format => :xhtml
+Slim::Engine.set_default_options :format => :xhtml,
+                                 :shortcut => {'&' => 'input type', '#' => 'id', '.' => 'class'}
