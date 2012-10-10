ed3056fa00637ed5647dadef15a6ce5f31ebb3b8
Daniel Mendler <mail@daniel-mendler.de>
2012-10-11 00:20:28 +0200

    rugged repository fixes

--- plugins/repositories/rugged/main.rb
+++ plugins/repositories/rugged/main.rb
@@ -54,23 +54,45 @@ class RuggedRepository < Repository
     #raise NotImplementedError
     version ||= @git.head.target
     version = version.to_s
-    [commit_to_version(@git.lookup(version)),
-     commit_to_version(@git.lookup(version)),
-     commit_to_version(@git.lookup(version))]
+
+    commits = []
+    walker = Rugged::Walker.new(@git)
+    walker.sorting(Rugged::SORT_TOPO)
+    walker.push(version)
+    walker.each do |c|
+      if path.blank? || c.tree[path] || c.tree[path + CONTENT_EXT] || c.tree[path + ATTRIBUTE_EXT]
+        commits << c
+        break if commits.size == 2
+      end
+    end
+
+    succ, newer = nil, nil
+    walker.reset
+    walker.sorting(Rugged::SORT_TOPO)
+    walker.push(@git.head.target)
+    walker.each do |c|
+      if path.blank? || c.tree[path] || c.tree[path + CONTENT_EXT] || c.tree[path + ATTRIBUTE_EXT]
+        if c == commits[0]
+          succ = newer
+          break
+        end
+        newer = c
+      end
+    end
+
+    [commit_to_version(commits[1]), # previous version
+     commit_to_version(commits[0]), # current version
+     commit_to_version(succ)] # next version
   end
 
   def get_children(path, version)
-    puts "get_children: #{path.inspect}"
     commit = @git.lookup(version.to_s)
     raise 'Not a commit' unless Rugged::Commit === commit
     if path.blank?
       commit.tree
     else
-      if ref = commit.tree[path]
-        @git.lookup(ref[:oid])
-      else
-        []
-      end
+      return [] unless ref = commit.tree[path]
+      @git.lookup(ref[:oid])
     end.map {|e| e[:name] }.reject {|name| reserved_name?(name) }
   end
 
@@ -78,22 +100,16 @@ class RuggedRepository < Repository
     commit = @git.lookup(version.to_s)
     raise 'Not a commit' unless Rugged::Commit === commit
     path += CONTENT_EXT if path.blank? || commit.tree[path][:type] == :tree
-    if ref = commit.tree[path]
-      @git.lookup(ref[:oid]).content
-    else
-      ''
-    end
+    return '' unless ref = commit.tree[path]
+    @git.lookup(ref[:oid]).content
   end
 
   def get_attributes(path, version)
     commit = @git.lookup(version.to_s)
     raise 'Not a commit' unless Rugged::Commit === commit
     path += ATTRIBUTE_EXT
-    if ref = commit.tree[path]
-      YAML.load(@git.lookup(ref[:oid]).content)
-    else
-      {}
-    end
+    return {} unless ref = commit.tree[path]
+    YAML.load(@git.lookup(ref[:oid]).content)
   end
 
   def diff(path, from, to)
@@ -110,10 +126,9 @@ class RuggedRepository < Repository
     name.ends_with?(ATTRIBUTE_EXT) || name.ends_with?(CONTENT_EXT)
   end
 
-
   def commit_to_version(commit)
     commit && Version.new(commit.oid, User.new(commit.author[:name], commit.author[:email]),
-                          Time.new(commit.time), commit.message, commit.parents.map(&:oid), commit == @git.head)
+                          Time.at(commit.time), commit.message, commit.parents.map(&:oid), commit.oid == @git.head.target)
   end
 end
 
