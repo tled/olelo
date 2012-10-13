e1cb773a8fd1c61f7e02a31601d5d36dd515a26c
Daniel Mendler <mail@daniel-mendler.de>
2012-10-13 14:57:08 +0200

    simplify code

--- lib/olelo/middleware/degrade_mime_type.rb
+++ lib/olelo/middleware/degrade_mime_type.rb
@@ -8,7 +8,7 @@ module Olelo
       def call(env)
         status, header, body = @app.call(env)
         if header['Content-Type'] =~ %r{application/xhtml\+xml} &&
-            !env['HTTP_ACCEPT'].to_s.split(',').map(&:strip).include?('application/xhtml+xml')
+            !env['HTTP_ACCEPT'].to_s =~ %r{application/xhtml\+xml}
           header['Content-Type'] = 'text/html'
         end
         [status, header, body]
