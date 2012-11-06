b2eef15577e144d4f1c6a37b9fd217204320cd2e
Daniel Mendler <mail@daniel-mendler.de>
2012-11-06 17:36:28 +0100

    fix issue #80

--- plugins/filters/markdown_nowiki.rb
+++ plugins/filters/markdown_nowiki.rb
@@ -1,15 +1,33 @@
 description 'Markdown nowiki filter'
 
+# Embeds indented markdown text blocks in <notags> tags
+# and adds <notags> around ``texts``
 Filter.create :markdown_nowiki do |context, content|
   output = ''
-  until content.blank?
-    case content
-    when /(\A( {4}|\t).*)|(\A``.*?``)|(\A`[^`]*`)/
-      output << "<notags>#{$&}</notags>"
-    when /(\A[^`\n]+)|(\A\n+)/
-      output << $&
+  block, state = nil, nil
+  content.each_line do |line|
+    if block
+      if line =~ /\A( {4}|\t)/
+        block << line
+        state = :in
+      elsif line =~ /\A\s*\Z/
+        block << line
+        state = :after if state == :in
+      elsif state == :after
+        output << "<notags>#{block}</notags>"
+        block = nil
+      else
+        block << line
+        line = block
+        block = nil
+      end
+    elsif line =~ /\A\s*\Z/
+      block, state = line, :before
     end
-    content = $'
+
+    unless block
+      output << line.gsub(/``.*?``|`[^`]*`/, '<notags>\0</notags>')
+    end
   end
   output
 end
