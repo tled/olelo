2423c9bbf55b986640b2b564f68fb15cb29f530f
Daniel Mendler <mail@daniel-mendler.de>
2012-11-21 08:45:22 +0100

    use simple juno api

--- plugins/utils/cache.rb
+++ plugins/utils/cache.rb
@@ -41,13 +41,7 @@ class Cache
   end
 
   def default_store
-    @@store ||=
-      begin
-        type = Config['cache_store.type']
-        klass = Juno.const_get(type) rescue nil
-        raise "Configure a valid cache_store: #{Juno.constants.join(', ')}" unless klass
-        klass.new(Config['cache_store'][type])
-      end
+    @@store ||= Juno.new(Config['cache_store.type'], Config['cache_store'][Config['cache_store.type']])
   end
 
   class Disabler
