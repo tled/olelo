4d984c51cbb162d9139621dce4cbf59cd588af69
TLed <tled@ledderboge.de>
2013-04-10 23:02:04 +0200

    quick'n'dirty salt+password storage

--- plugins/authentication/yamlfile.rb
+++ plugins/authentication/yamlfile.rb
@@ -19,7 +19,7 @@ class YamlfileService < User::Service
   def authenticate(name, password)
     @store.transaction(true) do |store|
       user = store[name]
-      raise AuthenticationError, :wrong_user_or_pw.t if !user || user['password'] != crypt(password)
+      raise AuthenticationError, :wrong_user_or_pw.t if !user || user['password'] != crypt(password,user['password'])
       User.new(name, user['email'], user['groups'])
     end
   end
@@ -30,7 +30,7 @@ class YamlfileService < User::Service
       raise :user_already_exists.t(name: user.name) if store[user.name]
       store[user.name] = {
         'email' => user.email,
-        'password' => crypt(password),
+        'password' => crypt(password,nil),
 	'groups' => user.groups.to_a
       }
     end
@@ -50,16 +50,34 @@ class YamlfileService < User::Service
     @store.transaction do |store|
       check do |errors|
         errors << 'User not found' if !store[user.name]
-        errors << :wrong_password.t if crypt(oldpassword) != store[user.name]['password']
+        errors << :wrong_password.t if crypt(oldpassword,store[user.name]['password']) != store[user.name]['password']
       end
-      store[user.name]['password'] = crypt(password)
+      store[user.name]['password'] = crypt(password,nil)
     end
   end
 
   private
 
-  def crypt(s)
-    s.blank? ? s : sha256(s)
+  def crypt(s,salt)
+    salty = false
+    # new password
+    if salt.nil?
+        #quick'n'dirty salt, nothing to rely on
+        salt = (0...20).map{(1+rand(127)).chr}.join
+        salt = sha256(salt)
+        salty = true
+    #salty password
+    elsif salt[0] == '$'
+        salt = salt.split('$')[1]
+        salty = true
+    end
+
+    if salty
+        return '$'+salt+'$'+sha256(s+salt)
+    else
+        #backward compatiblity
+        return sha256(s)
+    end
   end
 end
 
